74b3af14e1076316277789546e4d7aa5805a9858
adam-riot <75500582+adam-riot@users.noreply.github.com>
2025-07-03 18:29:39 +0800

    `Update NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY in next.config.js`

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-      NEXT_PUBLIC_SUPABASE_URL: 'https://bhilmyrvbfzosxntldwx.supabase.co', // <-- TAMBAH TANDA PETIK
-      NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoaWxteXJ2YmZ6b3N4bnRsZHd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE2NTQ2NSwiZXhwIjoyMDY2NzQxNDY1fQ.ug_OF6sbvjHWOlYn4x5ZjXxjyjWVaTlD-i5uGz-PucI', // <-- TAMBAH TANDA PETIK
+      NEXT_PUBLIC_SUPABASE_URL: 'https://bhilmyrvbfzosxntldwx.supabase.co', // Gantikan dengan URL sebenar anda
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoaWxteXJ2YmZ6b3N4bnRsZHd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExNjU0NjUsImV4cCI6MjA2Njc0MTQ2NX0.ug_OF6sbvjHWOlYn4x5ZjXxjyjWVaTlD-i5uGz-PucI', // Gantikan dengan kunci sebenar anda
     },
   };
   
