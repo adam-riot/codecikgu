15dafc32d5902bf7f0790bc24ead0a3746628727
adam-riot <75500582+adam-riot@users.noreply.github.com>
2025-08-09 14:22:27 +0800

    fix: remove X-Content-Type-Options header causing MIME type issues

--- next.config.js
+++ next.config.js
@@ -22,10 +22,6 @@ const nextConfig = {
             value: 'DENY',
           },
           {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
             key: 'Referrer-Policy',
             value: 'strict-origin-when-cross-origin',
           },
